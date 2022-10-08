20312d067fd0762f850dfdfd99655aa7e0965a83
André Saxton <andre_saxton@hotmail.com>
2022-10-07 21:25:50 -0700

    Cesta componentizada

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import { StatusBar, SafeAreaView, View } from 'react-native';
-import Cesta from './src/telas/Cesta';
 import { 
   useFonts, 
   Montserrat_400Regular, 
   Montserrat_700Bold 
 } from '@expo-google-fonts/montserrat'
+
+import Cesta from './src/telas/Cesta/';
 
 export default function App() {
 
