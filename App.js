80c58c0b09bfbb3fe5ccf6414d9dc9874fd6a7f3
André Saxton <andre_saxton@hotmail.com>
2022-10-07 21:58:48 -0700

    AppLoading e revisão

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
   Montserrat_400Regular, 
   Montserrat_700Bold 
 } from '@expo-google-fonts/montserrat'
+import AppLoading from 'expo-app-loading';
 
 import Cesta from './src/telas/Cesta/';
 import mock from './src/mocks/cesta';
@@ -19,7 +20,7 @@ export default function App() {
 
   if (!fonteCarregada){
     // Quando não tiver fonte carregada exibe tela em branco
-    return <View />
+    return <AppLoading />
   }
 
   // Carregando aplicação
