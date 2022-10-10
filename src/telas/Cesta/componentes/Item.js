a056cb8bc3d1a4d67220434026f220ab8957516b
André Saxton <andre_saxton@hotmail.com>
2022-10-09 17:47:49 -0700

    FlatList

--- src/telas/Cesta/componentes/Item.js
+++ src/telas/Cesta/componentes/Item.js
@@ -1,20 +1,13 @@
 import React from "react";
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, FlatList } from 'react-native';
 
 import Texto from "../../../componentes/Texto";
 
-export default function Itens({ titulo, lista }) {
-    return <>
-     <Texto style={estilos.titulo}>{ titulo }</Texto>
-     {
-        lista.map(({ nome, imagem }) => {
-            return <View key={nome} style={estilos.item}>
-                 <Image source={imagem} style={estilos.imagem} />
-                 <Texto style={estilos.nome}>{ nome }</Texto>
-                </View>
-        })
-     }
-    </>
+export default function Item({ item: { nome, imagem }}) {
+    return <View style={estilos.item}>
+        <Image source={imagem} style={estilos.imagem} />
+        <Texto style={estilos.nome}>{ nome }</Texto>
+    </View>
 }
 
 const estilos = StyleSheet.create({
@@ -31,6 +24,7 @@ const estilos = StyleSheet.create({
         borderBottomWidth: 1,
         borderBottomColor: "#ECECEC",
         paddingVertical: 16,
+        marginHorizontal: 16,
         alignItems: "center",
     },
     imagem: {
